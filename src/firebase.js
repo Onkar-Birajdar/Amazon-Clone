5cd46d855a678f0b6059972f5ae5276baca44afb
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-06-28 14:09:50 +0530

    Inital commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZbsGeSCKk9xwSnrwWUvAeNqas39GCKVA",
+    authDomain: "clone-clash-coder-3d740.firebaseapp.com",
+    projectId: "clone-clash-coder-3d740",
+    storageBucket: "clone-clash-coder-3d740.appspot.com",
+    messagingSenderId: "302347733586",
+    appId: "1:302347733586:web:9dbf2b5aa8913e9c7aad8b",
+    measurementId: "G-PTXHTR9RLX",
+};
