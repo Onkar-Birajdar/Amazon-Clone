b6b6875347790f3daea48d8e6546443f645b89a6
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-08-04 21:13:36 +0530

    Login page component Done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
-  return (
-    <div>
-      <h2>Hello, SingIn</h2>
-      <h2>SinUp</h2>
-      
-    </div>
-  )
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = (e) => {
+        e.preventDefault();
+    };
+    const register = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="login">
+            <Link to="/">
+                <img
+                    className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    alt="logo"
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        placeholder="Enter your email"
+                    />
+                    <h5>Password</h5>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        placeholder="Enter your strong password"
+                    />
+                    <button
+                        className="login__signInButton"
+                        type="submit"
+                        onClick={signIn}
+                    >
+                        Sign-in
+                    </button>
+                </form>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                    iure voluptate voluptates numquam perspiciatis voluptatum
+                    illo odio. Vero laboriosam labore repellendus debitis
+                    quaerat, eveniet cumque ut amet repudiandae voluptate
+                    ducimus.
+                </p>
+                <button
+                    className="login__registerButton"
+                    type="submit"
+                    onClick={register}
+                >
+                    Create your Amazon account
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
